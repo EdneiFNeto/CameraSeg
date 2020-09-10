469b4837b1edcfcc4a8b1564fe7ed838d4005446
edneifneto <edneifneto@gmail.com>
2020-09-10 13:45:59 -0300

    add login

--- app/src/main/java/com/example/camera/ui/SplachActivity.kt
+++ app/src/main/java/com/example/camera/ui/SplachActivity.kt
@@ -1,13 +1,20 @@
 package com.example.camera.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.camera.R
+import com.example.camera.ui.base.BaseActivity
 
-class SplachActivity : AppCompatActivity() {
+class SplachActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splach)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }, 3000)
     }
 }
---
+++ app/src/main/java/com/example/camera/ui/base/BaseActivity.kt
@@ -0,0 +1,7 @@
+package com.example.camera.ui.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity(){
+
+}
